fe28132939db445c3d9067175894921d486bc780
BaHoa-Nguyen <a1938499@student.adelaide.edu.au>
2026-08-24 15:48:59 +0930

    feat: add logging and customise transcription model

--- src/main/java/comp3011/assignment1/service/TranscriptionService.java
+++ src/main/java/comp3011/assignment1/service/TranscriptionService.java
@@ -1,9 +1,13 @@
 package comp3011.assignment1.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.audio.transcription.TranscriptionModel;
+import org.springframework.ai.openai.OpenAiAudioTranscriptionOptions;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+// for logging purposes
+@Slf4j
 @Service
 public class TranscriptionService {
 
@@ -14,9 +18,22 @@ public class TranscriptionService {
   }
 
   // the transcriptionModel class already have the transcribe method
-  public String transcribe(MultipartFile audio) {
+  public String transcribe(MultipartFile audioFile) {
 
-    return transcriptionModel.transcribe(audio.getResource());
+    try {
+      // using gpt-4o-transcribe for more accurate transcription
+      OpenAiAudioTranscriptionOptions options = OpenAiAudioTranscriptionOptions.builder()
+          .model("gpt-4o-transcribe")
+          .language("en") // default english
+          .temperature(0.0f) // temperature set at 0 for better accuracy
+          .build();
+
+      return transcriptionModel.transcribe(audioFile.getResource(), options);
+
+    } catch (Exception err) {
+      log.error("Transcription Failed: ", err);
+      throw err;
+    }
   }
 
 }
